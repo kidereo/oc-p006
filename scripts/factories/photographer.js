e4df9538c99e21bf65a207dab9cd89ac550654a0
kidereo <kirill.derevenski@gmail.com>
2022-04-20 14:04:27 +0200

    Data access and index view

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,63 @@
+/**
+ * Build up photographers' cards on index.
+ *
+ * @param data
+ * @returns {{name: *, getUserCardDOM: (function(): HTMLElement), picture: string}}
+ */
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    // Construct data
+    const {name, portrait, city, tagline, price} = data;
+    const picture = `media/Photographers_ID_Photos/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
-
+    /**
+     * Buld up individual photographer cards.
+     *
+     * @returns {HTMLElement}
+     */
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        const h2 = document.createElement( 'h2' );
+        // Declare individual elements
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        const h2 = document.createElement("h2");
+        const location = document.createElement("p");
+        const tline = document.createElement("p");
+        const fee = document.createElement("p");
+
+        // Hydrate individual elements
+        Object.assign(img, {
+            src: picture,
+            alt: "Photo of " + name
+        });
+        //img.setAttribute("src", picture);
+        //img.setAttribute("alt", "Photo of " + name);
+
         h2.textContent = name;
+
+        location.textContent = city;
+        location.classList.add("photographers-section-location");
+
+        tline.textContent = tagline;
+        tline.classList.add("photographers-section-tagline");
+
+        fee.textContent = price + "€/jour";
+        fee.classList.add("photographers-section-price");
+
+        // Attach individual elements
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tline);
+        article.appendChild(fee);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    return {
+        name,
+        picture,
+        city,
+        tagline,
+        price,
+        getUserCardDOM
+    };
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,33 +1,33 @@
+/**
+ * Connect to the datasource and return data arrays.
+ *
+ * @returns {Promise<{photographers: *[]}>}
+ */
 async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
-    const photographers = [
-        {
-            "name": "Ma data test",
-            "id": 1,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Ceci est ma data test",
-            "price": 400,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Autre data test",
-            "id": 2,
-            "city": "Londres",
-            "country": "UK",
-            "tagline": "Ceci est ma data test 2",
-            "price": 500,
-            "portrait": "account.png"
-        },
-    ]
-    // et bien retourner le tableau photographers seulement une fois
-    return ({
-        photographers: [...photographers, ...photographers, ...photographers]
-    })
+    // Connect to the datasource with fetch()
+    const source = "data/photographers.json";
+    const response = await fetch(source);
+    const data = await response.json();
+
+    // Distill the source into thematic arrays
+    const artistData = [...data.photographers];
+    const mediaData = [...data.media];
+
+    // Return data
+    return {
+        'photographers': artistData,
+        'media': mediaData
+    }
 }
 
+/**
+ * Display photographers with details as specified in the photographerFactory.
+ *
+ * @param photographers
+ * @returns {Promise<void>}
+ */
 async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
+    const photographersSection = document.querySelector(".photographers-section");
 
     photographers.forEach((photographer) => {
         const photographerModel = photographerFactory(photographer);
@@ -36,11 +36,17 @@ async function displayData(photographers) {
     });
 }
 
+/**
+ * Prepare for the page lift-off
+ *
+ * @returns {Promise<void>}
+ */
 async function init() {
     // Récupère les datas des photographes
     const {photographers} = await getPhotographers();
     displayData(photographers);
 }
 
+// We have a lift-off!
 init();
     
