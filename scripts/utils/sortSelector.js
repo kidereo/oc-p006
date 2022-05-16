f046ce8e717bc129e9337f9ae9b5e8c254a5193a
kidereo <kirill.derevenski@gmail.com>
2022-05-16 15:19:43 +0200

    Replace <radio> group with <select> on keyboard navigation

--- scripts/utils/sortSelector.js
+++ scripts/utils/sortSelector.js
@@ -46,3 +46,37 @@ const sortDate = document.getElementById('sort_date');
 sortTitle.onchange = sortByTitleDesc;
 sortLikes.onchange = sortByLikesDesc;
 sortDate.onchange = sortByDateDesc;
+
+
+/**
+ * Keyboard navigation of <select> element
+ */
+document.addEventListener('keydown', (event) => {
+    const radioSorter = document.querySelector('.sort_radio_container');
+    const selectSorter = document.querySelector('.sort_selector');
+    const prompt = document.querySelector(".sort_selector_nav_prompt");
+    const selector = document.getElementById('selector');
+    if (event.code === 'Tab') {
+        // Replace fancy dropdown with <select> element
+        radioSorter.style.display = 'none';
+        selectSorter.style.display = 'block';
+        // Display prompt to use keyboard arrows
+        //prompt.style.display = 'flex';
+    }
+});
+
+/**
+ * Keyboard navigation of <radio>-based dropdown.
+ */
+/*document.addEventListener('keydown', (event) => {
+    let sorter = document.querySelector('.contact_button');
+    let radioSet = document.querySelector('.radio_options');
+    const prompt = document.getElementById("sort_radio_container_nav_prompt");
+    if (sorter === document.activeElement || sortLikes === document.activeElement || sortDate === document.activeElement || sortTitle === document.activeElement) {
+        prompt.style.display = "flex";
+        radioSet.classList.add('radio_options_focus');
+    } else {
+        prompt.style.display = "none";
+        radioSet.classList.remove('radio_options_focus');
+    }
+});*/
